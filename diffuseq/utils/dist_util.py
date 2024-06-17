8a03725239a8a7c0cea38558aae3a89c3b0cbfbd
fangyubo <fyb0118@gmail.com>
2024-06-17 16:02:26 +0900

    fix: fix bugs that may cause decoding failed.

--- diffuseq/utils/dist_util.py
+++ diffuseq/utils/dist_util.py
@@ -46,7 +46,7 @@ def dev():
     """
     Get the device to use for torch.distributed.
     """
-    if th.cuda.is_available():
+    if th.cuda.is_available():  
         return th.device(f"cuda:{os.environ['LOCAL_RANK']}")
     return th.device("cpu")
 
---
+++ scripts/train_testing.sh
@@ -0,0 +1,20 @@
+CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=12231 --use_env run_train.py \
+--diff_steps 2000 \
+--lr 0.0001 \
+--learning_steps 50000 \
+--save_interval 5000 \
+--seed 102 \
+--noise_schedule sqrt \
+--hidden_dim 128 \
+--bsz 425 \
+--microbatch 425 \
+--dataset QQP \
+--data_dir /data2/fyb/QQP \
+--learned_mean_embed True \
+--denoise True \
+--vocab bert \
+--seq_len 128 \
+--use_fp16 \
+--denoise_rate 0.5 \
+--schedule_sampler lossaware \
+--notes learned_mask_fp16_denoise_0.5_reproduce
